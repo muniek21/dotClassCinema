c6dadc4c7ae04165e4a38f9391474e4e1c831409
muniek21 <zaba.mon@gmail.com>
2015-11-08 16:02:49 +0100

    Added Session model, log in and out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenicate(params[:session][:password])
-  	else
+  	   log_in user
+       redirect_to user
+    else
+      flash[:danger] = "Invalid email/password combnation"
   		render 'new'
   	end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
