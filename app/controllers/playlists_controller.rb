ac9d259a8956b216c323e3e675e58265a1aa59ee
KonradSotniczuk <konrad.sotniczuk@gmail.com>
2015-11-09 13:15:04 +0100

    Playlisty dodane. Viwes beda dodane.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,22 +1,39 @@
 class PlaylistsController < ApplicationController
+  
+#  before_action :set_playlist, only: [:show, :edit, :update, :destroy]
+#  before_action :set_user, only: [:new, :create]
+
   def index
+    @playlist = Playlist.all
   end
 
   def show
+    @playlist = Playlist.find(params[:id])
   end
 
   def new
+    @playlist = Playlist.new
   end
 
   def create
+    @playlist = Playlist.new(user_params)
   end
 
   def edit
+    @playlist = Playlist.find(params[:id])
   end
 
   def update
+    @playlist = Playlist.find(params[:id])
+      if @playlist.update(user_params)
+        redirect_to playlists_path(playlist)
+      else
+        render 'edit'
+      end    
   end
 
   def destroy
+      Playlist.find(params[:id]).destroy
+      redirect_to playlists_path(playlist)
   end
 end
