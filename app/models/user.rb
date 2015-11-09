af0b88cd4063591ff5c03480664bdaeec56d4e1f
muniek21 <zaba.mon@gmail.com>
2015-11-09 19:49:45 +0100

    konlikt

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
 
+	
+	validates :password, presence: true
 	validates :email, uniqueness: true
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
