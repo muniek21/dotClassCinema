fc034df179f7dc32bb178e6464ea5cd9a5e1a69b
KonradSotniczuk <konrad.sotniczuk@gmail.com>
2015-11-09 19:49:54 +0100

    konflikt3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
-
-
-	validates :email, presence: true, length: { maximum: 225 },
+wdwadadwdad
+wddadawdwadwa
+dwdwad
+wa
+wd
+wada
+	validatedwadawddadwadwas :email, presence: true, length: { maximum: 225 },
 				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
 
